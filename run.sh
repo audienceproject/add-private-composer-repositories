15d82a45278f2890715cf5c65d83c0b915200c97
Anamaria Todor <anamaria@userreport.com>
2017-01-05 12:26:16 +0100

    Fixed if logicx

--- run.sh
+++ run.sh
@@ -7,13 +7,16 @@ if [ ! -f $COMPOSER_HOME/config.json ]; then
 fi
 
 # check if repositories field exists, if not, add it
-if [ x`cat $COMPOSER_HOME/config.json | jq '.repositories'` -eq "null" ]; then
+REPOSITORIES_FIELD=`cat $COMPOSER_HOME/config.json | jq '.repositories'`
+if [ "$REPOSITORIES_FIELD" == "null" ]; then
+    echo "Repositories field not present. Creating..."
     cat $COMPOSER_HOME/config.json | jq '.repositories=[]' > result.json
     mv -f result.json $COMPOSER_HOME/config.json
 fi
 
 # check if repository entry already exists
-if [ x`cat $COMPOSER_HOME/config.json | jq --arg type $WERCKER_ADD_PRIVATE_COMPOSER_REPOSITORIES_TYPE --arg url $WERCKER_ADD_PRIVATE_COMPOSER_REPOSITORIES_URL '.repositories | map(contains({"type": $type, "url": $url})) | any'` -eq "true" ]; then
+DOES_ENTRY_EXIST=`cat $COMPOSER_HOME/config.json | jq --arg type $WERCKER_ADD_PRIVATE_COMPOSER_REPOSITORIES_TYPE --arg url $WERCKER_ADD_PRIVATE_COMPOSER_REPOSITORIES_URL '.repositories | map(contains({"type": $type, "url": $url})) | any'`
+if [ "$DOES_ENTRY_EXIST" == "true" ]; then
     echo "Private repository entry already present... ignoring."
 else
     # add repository entry
