28d740ba150e2e7e92008247b7b90d85e37a86b8
Anamaria Todor <anamaria@userreport.com>
2017-01-05 12:35:21 +0100

    Added properties

--- run.sh
+++ run.sh
@@ -15,11 +15,11 @@ if [ "$REPOSITORIES_FIELD" == "null" ]; then
 fi
 
 # check if repository entry already exists
-DOES_ENTRY_EXIST=`cat $COMPOSER_HOME/config.json | jq --arg type $WERCKER_ADD_PRIVATE_COMPOSER_REPOSITORIES_TYPE --arg url $WERCKER_ADD_PRIVATE_COMPOSER_REPOSITORIES_URL '.repositories | map(contains({"type": $type, "url": $url})) | any'`
+DOES_ENTRY_EXIST=`cat $COMPOSER_HOME/config.json | jq --arg type $WERCKER_ADD_PRIVATE_COMPOSER_REPOSITORY_TYPE --arg url $WERCKER_ADD_PRIVATE_COMPOSER_REPOSITORY_URL '.repositories | map(contains({"type": $type, "url": $url})) | any'`
 if [ "$DOES_ENTRY_EXIST" == "true" ]; then
     echo "Private repository entry already present... ignoring."
 else
     # add repository entry
-    cat $COMPOSER_HOME/config.json | jq --arg type $WERCKER_ADD_PRIVATE_COMPOSER_REPOSITORIES_TYPE --arg url $WERCKER_ADD_PRIVATE_COMPOSER_REPOSITORIES_URL  '.repositories |= .+ [{"type": $type, "url": $url}]' > result.json
+    cat $COMPOSER_HOME/config.json | jq --arg type $WERCKER_ADD_PRIVATE_COMPOSER_REPOSITORY_TYPE --arg url $WERCKER_ADD_PRIVATE_COMPOSER_REPOSITORY_URL  '.repositories |= .+ [{"type": $type, "url": $url}]' > result.json
     mv -f result.json $COMPOSER_HOME/config.json
 fi
